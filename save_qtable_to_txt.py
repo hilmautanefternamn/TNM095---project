fix: wrong dimensions for new states in script

save_qtable_to_txt.py
@@ -1,12 +1,12 @@
 import numpy as np 
 
 # init q-table
-Q_table = np.zeros([2**16, 4])	# [625,4]
+Q_table = np.zeros([625, 4])	# new: [625,4] , old: 2**16
 
 # load q-table from file
-loaded_qtable = np.load("filename.npy")
+loaded_qtable = np.load("q_table_lisa.npy")
 assert loaded_qtable.shape == Q_table.shape, "Q-table sizes do not agree"
 Q_table = loaded_qtable
 
 # save q-table to txt-file
-np.savetxt("filename.txt", Q_table, fmt="%4f", delimiter=", ", newline="\n")
+np.savetxt("qtable.txt", Q_table, fmt="%4f", delimiter=", ", newline="\n")